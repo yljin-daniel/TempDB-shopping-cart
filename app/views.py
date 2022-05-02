911d51f996612ec2f806dd537ba49a9e52f5a6cf
NikoCao <niko@nikocao.com>
2022-05-01 20:57:07 -0400

    added myacount feature

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from ast import For
+from cgi import print_exception
 from flask import Flask, request, session, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from app.forms import CustomerRegForm, LoginForm
@@ -6,23 +8,19 @@ import config
 import random
 import datetime
 
-# import sqlite3
-# con = sqlite3.connect('C:/Users/kingd/Downloads/code_Flask/code/19.OnlineCart/computer_store/db/database.db')
-# cur = con.cursor()
-
 app = Flask(__name__)
+# 从config模块中加载配信息
 app.config.from_object(config)
+# 创建SQLAlchemy对象db
 db = SQLAlchemy(app)
+currentOrder = 2
 
-
-
+# 注册实现
 @app.route('/reg', methods=['GET', 'POST'])
 def register():
     form = CustomerRegForm(request.form)
     if request.method == 'POST'and form.validate():
-        c=db.engine.execute('select * from Customers where id= ?', form.userid.data)
-        row = c.fetchone()
-        if row is None:
+            # 从表单上取出数据添加到Customer数据模型对象中
             new_customer = Customer()
             new_customer.id = form.userid.data
             new_customer.name = form.name.data
@@ -30,15 +28,14 @@ def register():
             new_customer.address = form.address.data
             new_customer.birthday = form.birthday.data
             new_customer.phone = form.phone.data
-            # insert into Customers values (?????)
-            db.session.add(new_customer) 
+
+            db.session.add(new_customer)
             db.session.commit()
 
-            print('Register Success')
+            print('注册成功')
             return render_template('customer_reg_success.html', form=form)
-        else:
-            flash('This id has been taken.')
-            return render_template('customer_reg.html', form=form)
+        # else:
+        #     return render_template('customer_reg.html', form=form)
 
     return render_template('customer_reg.html', form=form)
 
@@ -50,7 +47,7 @@ def login():
     if request.method == 'POST'and form.validate():
             c = db.session.query(Customer).filter_by(id=form.userid.data).first()
             if c is not None and c.password == form.password.data:
-                print('Login Success')
+                print('登录成功')
                 customer = {}
                 customer['id'] = c.id
                 customer['name'] = c.name
@@ -58,37 +55,61 @@ def login():
                 customer['address'] = c.address
                 customer['phone'] = c.phone
                 customer['birthday'] = c.birthday
+                # customer保持到HTTP Session
                 session['customer'] = customer
 
                 return redirect(url_for('main'))
             else:
-                flash('There may be errors in User id or Password.')
+                flash('您输入的客户账号和密码错误。')
                 return render_template('login.html', form=form)
     return render_template('login.html', form=form)
 
+@app.route('/account', methods=['GET', 'POST'])
+def show_account():
+    dt = session['customer']['name']
+    c = db.session.query(Orders).filter_by(user_name =session['customer']['name'])
+    msg = []
+    for i in c:
+        msg.append(i.id)
+        # msg += "\n order.id:" + str(i.id)
+    print("!!!!!!!!!!!")
+    print(msg)
+    return render_template('account.html', nm = dt, od = c)
 
 @app.route('/main')
 def main():
-    if 'customer' not in session.keys():
-        flash('Please Login.')
-        return redirect(url_for('login'))
+    # if 'customer' not in session.keys():
+    #     flash('您还没有登录，请登录。')
+    #     return redirect(url_for('login'))
     return render_template('main.html')
 
 
+# 显示商品列表
 @app.route('/list')
 def show_goods_list():
     if 'customer' not in session.keys():
-        flash('Please Login.')
+        flash('您还没有登录，请登录。')
         return redirect(url_for('login'))
 
     goodslist = db.session.query(Goods).all()
     return render_template('goods_list.html', list=goodslist)
 
+@app.route('/newList', methods=['GET', 'POST'])
+def show_goods_list_new():
+    if 'customer' not in session.keys():
+        flash('您还没有登录，请登录。')
+        return redirect(url_for('login'))
+    ft = 999999999
+    if request.method == 'POST':
+        ft = request.form.get('user')
+    goodslist = db.session.query(Goods).filter(Goods.price<ft).all()
+    return render_template('goods_list.html', list=goodslist)
 
+# 显示商品详细
 @app.route('/detail')
 def show_goods_detail():
     if 'customer' not in session.keys():
-        flash('Please Login.')
+        flash('您还没有登录，请登录。')
         return redirect(url_for('login'))
 
     goodsid = request.args['id']
@@ -97,45 +118,49 @@ def show_goods_detail():
     return render_template('goods_detail.html', goods=goods)
 
 
+# 添加购物车
 @app.route('/add')
 def add_cart():
     if 'customer' not in session.keys():
-        flash('Please Login.')
+        flash('您还没有登录，请登录。')
         return redirect(url_for('login'))
 
     goodsid = int(request.args['id'])
     goodsname = request.args['name']
     goodsprice = float(request.args['price'])
 
-    # check if cart has been created in this Session
+    # 判断Session中是否有购物车数据
     if 'cart' not in session.keys():
-        session['cart'] = []  
+        session['cart'] = []  # 购物车是一个列表结构
 
     cart = session['cart']
-    # 0 item not in cart ,1 item already in cart
+    # 声明flag标志,如果0表示购物车中没有当前商品,1表示购物车总有当前商品
     flag = 0
     for item in cart:
-        if item[0] == goodsid:  # goods id
-            item[3] += 1  # item count ++
+        if item[0] == goodsid:  # item[0]保存在购物车的商品id
+            item[3] += 1  # item[3]保存在购物车的商品数量,对当前数量+1
             flag = 1
             break
 
     if flag == 0:
-        # initialized item first added to 1
+        # 第一次添加商品到购物车数量是1
         cart.append([goodsid, goodsname, goodsprice, 1])
 
     session['cart'] = cart
 
     print(cart)
 
-    flash('Item [' + goodsname + '] has added to your Cart')
+    flash('已经添加商品【' + goodsname + '】到购物车')
     return redirect(url_for('show_goods_list'))
 
 
+
+
+# 查看购物车
 @app.route('/cart')
 def show_cart():
     if 'customer' not in session.keys():
-        flash('Please Login.')
+        flash('您还没有登录，请登录。')
         return redirect(url_for('login'))
 
     if 'cart' not in session.keys():
@@ -145,6 +170,8 @@ def show_cart():
     list = []
     total = 0.0
     for item in cart:
+        # 购物车每一个元素[商品id, 商品名称, 商品价格, 商品数量]
+        # 添加一个小计
         subtotal = item[2] * item[3]
         total += subtotal
         new_item = (item[0], item[1], item[2], item[3], subtotal)
@@ -153,19 +180,23 @@ def show_cart():
     return render_template('cart.html', list=list, total=total)
 
 
-
+# 提交订单
 @app.route('/submit_order', methods=['POST'])
 def submit_order():
+    # 从表单中取出数据添加到Orders模式对象中
     orders = Orders()
-    # Order id is pruduced by time stamp and one random digit
+    # 生成订单id,规则当前时间戳+一位随机数
     n = random.randint(0, 9)
     d = datetime.datetime.today()
     orderid = str(int(d.timestamp() * 1e6)) + str(n)
     orders.id = orderid
+    orders.user_name = session['customer']['name']
     orders.orderdate = d.strftime('%Y-%m-%d %H:%M:%S')
-    orders.status = 1  # 1 Unpaid 0 Paid
-
+    orders.status = 1  # 1 待付款 0 已付款
+    currentOrder = orderid
+    print("This is order ID!" + currentOrder)
     db.session.add(orders)
+    # 购物车每一个元素[商品id, 商品名称, 商品价格, 商品数量]
     cart = session['cart']
     total = 0.0
     for item in cart:
@@ -175,6 +206,7 @@ def submit_order():
         except:
             quantity = 0
 
+        # 小计
         subtotal = item[2] * quantity
         total += subtotal
 
@@ -187,9 +219,10 @@ def submit_order():
         db.session.add(order_line_item)
 
     orders.total = total
+    # 提交事务
     db.session.commit()
 
-    # Empty cart
+    # 清除购物车
     session.pop('cart', None)
 
     return render_template('order_finish.html', orderid=orderid)
